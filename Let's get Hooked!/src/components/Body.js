b27e198246fde425da0c32f9ac5683a750a1fb76
Shilpa Sonule <103277952+shilpai@users.noreply.github.com>
2024-03-01 20:03:18 +0530

    json format updated
    minor changes was done

--- Let's get Hooked!/src/components/Body.js
+++ Let's get Hooked!/src/components/Body.js
@@ -1,11 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
   // Local State Variable
-  const [listOfRestaurant, setListOfRestaurant] = useState(restaurantList);
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5702448&lng=73.88233509999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  };
 
   return (
     <div className="body">
@@ -24,7 +36,7 @@ const Body = () => {
       </div>
       <div className="restaurant-container">
         {listOfRestaurant.map((restaurant) => (
-          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
